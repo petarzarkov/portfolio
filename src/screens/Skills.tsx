d43580106cb6df1bc1bf8d7afd1565a6d85d3226
Petar Zarkov <46219231+petarzarkov@users.noreply.github.com>
2022-03-10 18:12:33 +0200

    add-level-to-skills

--- src/screens/Skills.tsx
+++ src/screens/Skills.tsx
@@ -1,14 +1,31 @@
 import React, { FC } from "react";
 import { ReactElement } from "react";
-import { Box, SimpleGrid, Icon, Text, Stack, Flex } from "@chakra-ui/react";
+import { Box, SimpleGrid, Icon, Text, Stack, Flex, HStack } from "@chakra-ui/react";
 import { SiTypescript, SiJavascript, SiReact, SiPython, SiJava } from "react-icons/si";
 import { IoLogoNodejs } from "react-icons/io";
+import { MdSignalCellular1Bar, MdSignalCellular2Bar, MdSignalCellular3Bar, MdSignalCellular4Bar } from "react-icons/md";
+import { IconBaseProps } from "react-icons";
 
 const Skill: FC<{
   title: string;
   text?: string;
+  level?: 1 | 2 | 3 | 4;
   icon: ReactElement;
-}> = ({ title, text, icon }) => {
+}> = ({ title, text, icon, level = 1 }) => {
+  const Level: FC<{ lProps: IconBaseProps }> = ({ lProps }) => {
+    switch (level) {
+      case 1:
+        return <MdSignalCellular1Bar {...lProps} />;
+      case 2:
+        return <MdSignalCellular2Bar {...lProps} />;
+      case 3:
+        return <MdSignalCellular3Bar {...lProps} />;
+      case 4:
+        return <MdSignalCellular4Bar {...lProps} />;
+      default:
+        return <MdSignalCellular1Bar {...lProps} />;
+    }
+  };
   return (
     <Stack>
       <Flex
@@ -23,6 +40,12 @@ const Skill: FC<{
         {icon}
       </Flex>
       <Text fontWeight={600}>{title}</Text>
+      <HStack spacing={0}>
+        <Level lProps={{
+          color: "green",
+          size: 45
+        }} />
+      </HStack>
       {text ? <Text color={"gray.600"}>{text}</Text> : null}
     </Stack>
   );
@@ -31,34 +54,41 @@ const Skill: FC<{
 export const Skills: FC = () => {
   return (
     <Box p={4}>
-      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
+      <SimpleGrid columns={{ base: 2, md: 3, lg: 4 }} spacing={10}>
         <Skill
           icon={<Icon as={IoLogoNodejs} w={10} h={10} color={"green.600"} />}
           title={"NodeJS"}
+          level={4}
         />
         <Skill
           icon={<Icon as={SiTypescript} w={10} h={10} color={"blue.600"} />}
           title={"Typescript"}
+          level={3}
         />
         <Skill
           icon={<Icon as={SiJavascript} w={10} h={10} color={"#f0db4f"} />}
           title={"JavaScript"}
+          level={4}
         />
         <Skill
           icon={<Icon as={SiReact} w={10} h={10} color={"blue.200"} />}
           title={"React"}
+          level={2}
         />
         <Skill
           icon={<Icon as={SiReact} w={10} h={10} color={"blue.300"} />}
           title={"React Native"}
+          level={2}
         />
         <Skill
           icon={<Icon as={SiPython} w={10} h={10} color={"green.600"} />}
           title={"Python"}
+          level={2}
         />
         <Skill
           icon={<Icon as={SiJava} w={10} h={10} color={"red.600"} />}
           title={"Java"}
+          level={1}
         />
       </SimpleGrid>
     </Box>
